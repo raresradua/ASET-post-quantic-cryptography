ad43601e285241d39a7bb31dc8e50d51ac289fd1
mariacoteanu <coteanu.maria29@gmail.com>
2022-11-08 21:45:12 +0200

    create poly for goppa
    move from sage to .py and create poly for goppa initialization

--- proj/codes/goppa_code.py
+++ proj/codes/goppa_code.py
@@ -1,22 +1,63 @@
 from proj.codes.linear_code import LinearCode
+from galois import GF
 import numpy as np
 
 
 class GoppaCode(LinearCode):
-
-    def __init__(self, code):
+    def __init__(self, n, k, d, code=None, m=2, t=3, p=2):
         super().__init__(code)
-        self.L = np.array([])
-        self.C = np.matrix([][0])
+        if code:
+            self.m = m
+            self.F = GF(p ^ m)
+            self.t = t
+            self.L = []
+            self.g_factors = []
+            self.code = self.generate_code()
+            print("Generated Code: ", self.code)
+        else:
+            self.code = code
 
-    def g(self, x_val):
-        pass
+    def accept_code(self, n, k, d):
+        if k < n - self.m * self.t:
+            return False
+        if d < 2 * self.t:
+            return False
+        return True
 
-    def R(self, c_vector, x_integer):
-        pass
+    def get_parity_check_matrix(self):
+        return self.code.parity_check_matrix()
 
-    def set_code(self, code):
-        pass
+    def generate_code(self):
+        g = self.generate_goppa_polynomial_v1()
+        self.poly = g
+        L = [a for a in self.F.list() if g(a) != 0]
+        return self.code.GoppaCode(g, L)
 
-    def get_code(self, code):
-        pass
+    def generate_goppa_polynomial_v1(self):
+        # TBD: we do not need this anymore because we implement g function
+        # the poly is irreducible when accept_code function returns True and that is an Goppa code
+        PolyRing = PolynomialRing(self.F, 'x')
+        return PolyRing.irreducible_element(self.t)
+
+    def g(self, value):
+        if len(self.g_factors) == 0:
+            i = 0
+            while i < self.t:
+                g = self.F.random_element()
+                self.g_factors.append(g)
+                i = i + 1
+        sum = 0
+        for i in range(self.t):
+            sum = sum + self.g_factors[i] * (value ** i)
+        return sum
+
+    def determine_L_set(self, n):
+        i = 0
+        while 1:
+            alpha = self.F.random_element()
+            if self.g(alpha):
+                if i < n:
+                    self.L.append(alpha)
+                    i = i + 1
+                else:
+                    break
