dca2401654aaa3a208e0d40ae0043c6097aab1d7
iulian2000-cmyk <67906958+iulian2000-cmyk@users.noreply.github.com>
2022-11-18 18:35:20 +0200

    Add files via upload

--- proj/codes/goppa_code_gen.py
+++ proj/codes/goppa_code_gen.py
@@ -1,7 +1,7 @@
 from linear_code import LinearCode
 import galois
 import numpy as np
-from sympy import Matrix
+from sympy import Matrix,randMatrix,zeros
 from sympy.abc import x, alpha
 import random as rand
 from sympy.polys.galoistools import gf_gcdex
@@ -29,16 +29,19 @@ def modinv(a, m):
 class GoppaCode:
     
     
-    def __init__(self, m=2, t=3, base_field=2, n=12):
+    def __init__(self, m=2, t=3, base_field=2, n=12,k=10):
         self.m = m
         self.t = t
         self.n = n
         self.base_field = base_field
         self.val = base_field ** m
+        self.k = k 
         self.F = galois.GF(base_field ** m)
         self.g = galois.irreducible_poly(self.F.order, self.t, 'random')
         self.coefficients = self.g.coefficients(self.t + 1, 'asc')
         self.H = self.get_parity_check_matrix()
+        self.G = self.get_generator_matrix()
+        
         
     
     def get_parity_check_matrix(self): 
@@ -53,7 +56,31 @@ class GoppaCode:
         Y = Matrix(self.t, self.n, lambda i, j: (alpha_set[j] ** i) % self.val)
         Z = Matrix(self.n, self.n, lambda i, j: modinv(int(self.g(alpha_set[i])) % int(self.val),int(self.val)) if i==j else 0)
         H = X * Y * Z
+        H = Matrix(H.shape[0],H.shape[1],lambda i,j: H[i,j] % self.val )
+        self.H = H
         return H 
+    
+     
+    def get_generator_matrix(self):
+        H_bin = Matrix(self.H.shape[0],self.H.shape[1],lambda i,j : self.H[i,j] % 2 )
+        G = []
+        for i in range(len(H_bin.nullspace())):
+            vec = []
+            for j in range(len(H_bin.nullspace()[i])):
+                vec.append(H_bin.nullspace()[i][j])
+            G.append(vec)
+        
+        G = Matrix(G)
+        return G 
+            
+        
+        
+        
+        
+        
+                
+        
+        
 
 
 obj = GoppaCode(13, 13, 2, 12)
