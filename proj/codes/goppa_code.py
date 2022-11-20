8baf86fcc9a9d64cbce4154f9b99af4259aadfbe
iulian2000-cmyk <67906958+iulian2000-cmyk@users.noreply.github.com>
2022-11-20 16:10:37 +0200

    Add files via upload

--- proj/codes/goppa_code.py
+++ proj/codes/goppa_code.py
@@ -1,12 +1,9 @@
 from linear_code import LinearCode
 import galois
-import numpy as np
-from sympy import Matrix, randMatrix, zeros
-from sympy.abc import x, alpha
+from sympy import Matrix
 import random as rand
-from sympy.polys.galoistools import gf_gcdex
-from sympy.polys.domains import ZZ
-from abc import ABC, abstractmethod
+from proj.utilities.utilities import consumed_memory, resource_measurement_aspect, time_measurement_aspect
+
 
 def extended_gcd(aa, bb):
     lastremainder, remainder = abs(aa), abs(bb)
@@ -14,8 +11,8 @@ def extended_gcd(aa, bb):
     while remainder:
         lastremainder, (quotient, remainder) = remainder, divmod(
             lastremainder, remainder)
-        x, lastx = lastx - quotient*x, x
-        y, lasty = lasty - quotient*y, y
+        x, lastx = lastx - quotient * x, x
+        y, lasty = lasty - quotient * y, y
     return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
 
 
@@ -29,7 +26,7 @@ def modinv(a, m):
 class BinarryGoppaCode(LinearCode):
 
     def __init__(self, m, t, n, k):
-        if k>=n-m*t:
+        if k >= n - m * t:
             self.m = m
             self.t = t
             self.n = n
@@ -41,40 +38,58 @@ class BinarryGoppaCode(LinearCode):
             self.coefficients = self.g.coefficients(self.t + 1, 'asc')
             self.H = self.get_parity_check_matrix()
             self.G = self.get_generator_matrix()
+            super().__init__(n, k, self)
         else:
             print("Invalid arguments ! ")
-        
 
-
-    
+    @consumed_memory
+    @resource_measurement_aspect
+    @time_measurement_aspect
     def get_parity_check_matrix(self):
-        alpha_set=[]
+        alpha_set = []
         while len(alpha_set) < self.n:
-            element=rand.randint(0, self.val)
-            if int(self.g(element)) % int(self.val) != 0 and modinv(int(self.g(element)) % int(self.val), int(self.val)) != None:
+            element = rand.randint(0, self.val)
+            if int(self.g(element)) % int(self.val) != 0 and modinv(int(self.g(element)) % int(self.val),
+                                                                    int(self.val)) is not None:
                 alpha_set.append(element)
 
-        X=Matrix(self.t, self.t, lambda i, j: int(self.coefficients[j - i]) if 0 <= i-j < self.t else 0)
-        Y=Matrix(self.t, self.n, lambda i, j: (alpha_set[j] ** i) % self.val)
-        Z=Matrix(self.n, self.n, lambda i, j: modinv(int(self.g(alpha_set[i])) % int(self.val), int(self.val)) if i == j else 0)
-        H=X * Y * Z
-        H=Matrix(H.shape[0], H.shape[1], lambda i, j: H[i, j] % self.val)
-        self.H=H
-        return H
+        x_matrix = Matrix(self.t, self.t, lambda i, j: int(
+            self.coefficients[j - i]) if 0 <= i - j < self.t else 0)
+        y_matrix = Matrix(self.t, self.n, lambda i, j: (alpha_set[j] ** i) % self.val)
+        z_matrix = Matrix(self.n, self.n, lambda i, j: modinv(
+            int(self.g(alpha_set[i])) % int(self.val), int(self.val)) if i == j else 0)
+        h_matrix = x_matrix * y_matrix * z_matrix
+        h_matrix = Matrix(h_matrix.shape[0], h_matrix.shape[1], lambda i, j: h_matrix[i, j] % self.val)
+        return h_matrix
 
-
+    @consumed_memory
+    @resource_measurement_aspect
+    @time_measurement_aspect
     def get_generator_matrix(self):
-        H_bin=Matrix(self.H.shape[0], self.H.shape[1],
-                     lambda i, j: self.H[i, j] % 2)
-        G=[]
-        for i in range(len(H_bin.nullspace())):
-            vec=[]
-            for j in range(len(H_bin.nullspace()[i])):
-                vec.append(abs(H_bin.nullspace()[i][j]))
-            G.append(vec)
+        if self.H is not None:
+            h_bin = Matrix(self.H.shape[0], self.H.shape[1], lambda i, j: self.H[i, j] % 2)
+            g_matrix = []
+            for i in range(len(h_bin.nullspace())):
+                vec = []
+                for j in range(len(h_bin.nullspace()[i])):
+                    vec.append(h_bin.nullspace()[i][j])
+                g_matrix.append(vec)
+            g_matrix = Matrix(g_matrix)
+            return g_matrix
+        else:
+            return None
 
-        G=Matrix(G)
-        return G
+    @consumed_memory
+    @resource_measurement_aspect
+    @time_measurement_aspect
+    def error_correction(self, codeword):
+        return None
 
-   
-obj=BinarryGoppaCode(13, 13,12,10)
+    @consumed_memory
+    @resource_measurement_aspect
+    @time_measurement_aspect
+    def decode(self, codeword):
+        return None
+
+
+obj = BinarryGoppaCode(13, 13, 12, 10)
--- proj/codes/reed_solomon.py
+++ proj/codes/reed_solomon.py
@@ -1,37 +1,38 @@
-from abc import ABC, abstractmethod
+from linear_code import LinearCode
+import galois
 from proj.utilities.utilities import consumed_memory, resource_measurement_aspect, time_measurement_aspect
+from abc import abstractmethod
 
 
-class LinearCode(ABC):
-    def __init__(self, n, k, code):
+class ReedSolomon(LinearCode):
+
+    def __int__(self, n, k):
         self.n = n
         self.k = k
-        self.code = code
+        self.reed_solomon = galois.ReedSolomon(n, k)
 
     @consumed_memory
     @resource_measurement_aspect
     @time_measurement_aspect
     @abstractmethod
     def get_parity_check_matrix(self):
-        ...
+        return self.reed_solomon.H
 
     @consumed_memory
     @resource_measurement_aspect
     @time_measurement_aspect
     @abstractmethod
     def get_generator_matrix(self):
-        ...
+        return self.reed_solomon.G
 
     @consumed_memory
     @resource_measurement_aspect
     @time_measurement_aspect
-    @abstractmethod
     def error_correction(self, codeword):
-        ...
+        return self.reed_solomon.detect(codeword)
 
     @consumed_memory
     @resource_measurement_aspect
     @time_measurement_aspect
-    @abstractmethod
     def decode(self, codeword):
-        ...
+        return self.reed_solomon.decode(codeword)
