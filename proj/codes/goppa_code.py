66464f307c55866c687d0bc81a948d671e1b0161
iulian2000-cmyk <67906958+iulian2000-cmyk@users.noreply.github.com>
2022-12-01 11:04:22 +0200

    Add files via upload

--- proj/codes/goppa_code.py
+++ proj/codes/goppa_code.py
@@ -1,57 +1,84 @@
 from linear_code import LinearCode
 import galois
-from sympy import Matrix, symbols, sqf_list, div
+from sympy import Matrix, symbols, sqf_part
 import random as rand
 from proj.utilities.utilities import consumed_memory, resource_measurement_aspect, time_measurement_aspect
 import logging
+import numpy as np
 
 logger = logging.getLogger()
 
 
+def random_perm_matrix(size):
+    return np.array([[1 if i == x else 0 for i in range(size)] for x in np.random.permutation(n)])
+
+
+def get_random_msg(size):
+    return [rand.randint(0, 1) for index in range(size)]
+
+
+def random_inv_matrix(size):
+    while 1:
+        candidate = np.random.randint(2, size=(size, size))
+        det = int(round(np.linalg.det(candidate)))
+        if det % 2 == 1:
+            return candidate
+
+
+def pow_(a, b):
+    r = a
+    for i in range(b):
+        r = r * a
+    return r
+
+
 def extended_gcd(aa, bb):
-    lastremainder, remainder = abs(aa), abs(bb)
-    x, lastx, y, lasty = 0, 1, 1, 0
+    last_remainder, remainder = abs(aa), abs(bb)
+    x, last_x, y, last_y = 0, 1, 1, 0
     while remainder:
-        lastremainder, (quotient, remainder) = remainder, divmod(
-            lastremainder, remainder)
-        x, lastx = lastx - quotient * x, x
-        y, lasty = lasty - quotient * y, y
-    return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
+        last_remainder, (quotient, remainder) = remainder, divmod(
+            last_remainder, remainder)
+        x, last_x = last_x - quotient * x, x
+        y, last_y = last_y - quotient * y, y
+    return last_remainder, last_x * (-1 if aa < 0 else 1), last_y * (-1 if bb < 0 else 1)
 
 
-def modinv(a, m):
-    g, x, y = extended_gcd(a, m)
+def mod_inv(a, mod):
+    g, x, y = extended_gcd(a, mod)
     if g != 1:
         return None
-    return x % m
+    return x % mod
 
 
 class BinaryGoppaCode(LinearCode):
 
-    def __init__(self, m, t, n, k):
+    def __init__(self, m_val, t_val, n_val, k_val):
         logger.info('[INFO] Calling __init__ for GoppaCode')
         try:
-            assert k >= n - m * t
+            assert k_val >= n_val - m_val * t_val
         except Exception:
             logger.error('[ERROR] Invalid arguments.')
         else:
             logger.info('[INFO] Arguments are alright.')
-            self.m = m
-            self.t = t
-            self.n = n
+            self.m = m_val
+            self.t = t_val
+            self.n = n_val
             self.base_field = 2
-            self.val = 2 ** m
-            self.k = k
-            self.F = galois.GF(2 ** m)
+            self.val = 2 ** m_val
+            self.k = k_val
+            self.F = galois.GF(2 ** m_val)
             self.g = galois.irreducible_poly(self.F.order, self.t, 'random')
             self.coefficients = self.g.coefficients(self.t + 1, 'asc')
             self.alpha_set = self.get_alpha_set()
             self.H = self.get_parity_check_matrix()
             self.G = self.get_generator_matrix()
-            self.syndrome_polynom = self.get_syndrome_polynom([1] * self.n)
-            self.error_correction([1] * self.n)
-
+            self.syndrome_polynom = self.get_syndrome_polynom([1, 1, 1, 1, 1, 1, 1])
+            self.e = self.error_correction([1, 1, 1, 1, 1, 1, 1])
             super().__init__(n, k, self)
+            logger.info('Arguments for Goppa Code: {} {} {} {} {} {} {} {} {} {} {} {} {} {}'.format(
+                self.m, self.t, self.n, self.base_field, self.val, self.k, self.F, self.g, self.coefficients,
+                self.alpha_set, self.H, self.H, self.G, self.syndrome_polynom
+            ))
 
     @consumed_memory
     @resource_measurement_aspect
@@ -61,8 +88,8 @@ class BinaryGoppaCode(LinearCode):
         alpha_set = []
         while len(alpha_set) < self.n:
             element = rand.randint(0, self.val)
-            if int(self.g(element)) % int(self.val) != 0 and modinv(int(self.g(element)) % int(self.val),
-                                                                    int(self.val)) is not None:
+            if int(self.g(element)) % int(self.val) != 0 and mod_inv(int(self.g(element)) % int(self.val),
+                                                                     int(self.val)) is not None:
                 alpha_set.append(element)
         logger.info('[INFO] Alpha set value: {}'.format(alpha_set))
         return alpha_set
@@ -75,7 +102,7 @@ class BinaryGoppaCode(LinearCode):
         x_matrix = Matrix(self.t, self.t, lambda i, j: int(
             self.coefficients[j - i]) if 0 <= i - j < self.t else 0)
         y_matrix = Matrix(self.t, self.n, lambda i, j: (self.alpha_set[j] ** i) % self.val)
-        z_matrix = Matrix(self.n, self.n, lambda i, j: modinv(
+        z_matrix = Matrix(self.n, self.n, lambda i, j: mod_inv(
             int(self.g(self.alpha_set[i])) % int(self.val), int(self.val)) if i == j else 0)
         h_matrix = x_matrix * y_matrix * z_matrix
         h_matrix = Matrix(h_matrix.shape[0], h_matrix.shape[1], lambda i, j: h_matrix[i, j] % self.val)
@@ -94,7 +121,7 @@ class BinaryGoppaCode(LinearCode):
             return None
         else:
             logger.info('[INFO] Everything is alright.')
-            h_bin = Matrix(self.H.shape[0], self.H.shape[1], lambda i, j: self.H[i, j] % 2)
+            h_bin = Matrix(self.H.shape[0], self.H.shape[1], lambda index_i, index_j: self.H[index_i, index_j] % 2)
             g_matrix = []
             for i in range(len(h_bin.nullspace())):
                 vec = []
@@ -115,28 +142,30 @@ class BinaryGoppaCode(LinearCode):
             poly = poly + x ** (len(polynom.coeffs) - 1 - i) * polynom.coeffs[i]
         return poly
 
-    # TO DO
     @consumed_memory
     @resource_measurement_aspect
     @time_measurement_aspect
     def get_syndrome_polynom(self, codeword):
         logger.info('[INFO] Calling get_syndrome_polynom')
         x = symbols('x')
+        s = 0
         g = self.transform_polynom(self.g)
-        syndrome_polynom = x * 0
         for i in range(len(codeword)):
-            syndrome_polynom = syndrome_polynom + codeword[i] * (x - self.alpha_set[i]).invert(g)
-        return syndrome_polynom
+            s = s + codeword[i] * (x - self.alpha_set[i]).invert(g)
+        return s
 
     @consumed_memory
     @resource_measurement_aspect
     @time_measurement_aspect
     def error_correction(self, codeword):
         x = symbols('x')
-        syndrome_polynom = self.get_syndrome_polynom(codeword)
-        g = self.transform_polynom(self.g)
-        h_polynom = syndrome_polynom.invert(self.transform_polynom(self.g))
-        quotient_polynom, tmp_polynom = div(h_polynom + x * 1, g)
+        g_x = self.transform_polynom(self.g)
+        S_polynom = self.get_syndrome_polynom(codeword)
+        if S_polynom == 0:
+            return codeword
+        H_polynom = S_polynom.invert(g_x)
+        quotient, T_polynom = divmod(sqf_part(H_polynom + x), g_x)
+        T_polynom = T_polynom.__getnewargs__()[0]
         return None
 
     @consumed_memory
@@ -147,4 +176,12 @@ class BinaryGoppaCode(LinearCode):
 
 
 if __name__ == '__main__':
-    obj = BinaryGoppaCode(13, 13, 12, 10)
+    m = 13
+    t = 13
+    n = 12
+    k = 10
+    code = BinaryGoppaCode(m, t, n, k)
+    S = random_inv_matrix(k)
+    P = random_perm_matrix(n)
+    G_ = S @ code.get_generator_matrix() @ P
+    m = get_random_msg(k)
